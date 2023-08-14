92dcf79b9d09604ee28d0e83d17d0e8830aa5156
Stepan Haida <stepanhaida@gmail.com>
2023-08-14 15:13:09 +0300

    add mybike-landing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const formBlock = document.querySelector('.form');
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+formBlock.addEventListener('submit', (event) => {
+  formBlock.reset();
+  event.preventDefault();
+});
